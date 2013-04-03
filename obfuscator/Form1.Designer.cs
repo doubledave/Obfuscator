a3dad9a45edb72ca50f63bef0f5d6e78f14e4b8a
David Mulgrew <david@boxxy.info>
2013-04-02 22:06:51 -0500

    commit the project, first time

---
+++ obfuscator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace obfuscator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSender = new System.Windows.Forms.TextBox();
+            this.buttonObfuscate = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(161, 122);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input text, 160 characters max";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Characters remaining: 160";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "encryption key";
+            // 
+            // textBoxSender
+            // 
+            this.textBoxSender.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxSender.Location = new System.Drawing.Point(13, 184);
+            this.textBoxSender.MaxLength = 14;
+            this.textBoxSender.Name = "textBoxSender";
+            this.textBoxSender.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSender.TabIndex = 4;
+            this.textBoxSender.Text = "123";
+            // 
+            // buttonObfuscate
+            // 
+            this.buttonObfuscate.Location = new System.Drawing.Point(180, 76);
+            this.buttonObfuscate.Name = "buttonObfuscate";
+            this.buttonObfuscate.Size = new System.Drawing.Size(100, 23);
+            this.buttonObfuscate.TabIndex = 7;
+            this.buttonObfuscate.Text = "Obfuscate";
+            this.buttonObfuscate.UseVisualStyleBackColor = true;
+            this.buttonObfuscate.Click += new System.EventHandler(this.buttonObfuscate_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(180, 105);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(100, 23);
+            this.buttonDecrypt.TabIndex = 8;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox4.Location = new System.Drawing.Point(286, 25);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(161, 122);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Output:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(283, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Characters: 0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 272);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(434, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 307);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonObfuscate);
+            this.Controls.Add(this.textBoxSender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSender;
+        private System.Windows.Forms.Button buttonObfuscate;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ obfuscator/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace obfuscator
+{
+    public partial class Form1 : Form
+    {
+        public System.Collections.ArrayList listBox1 = new System.Collections.ArrayList();
+        public int[] statistics = new int[32];
+        Random rnd = new Random();
+        int MaxMsg = 60000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonObfuscate.Enabled = true;
+            buttonDecrypt.Enabled = true;
+            progressBar1.Visible = false;
+            label1.Text = "Input text, "+MaxMsg.ToString()+" characters max";
+            label2.Text = "Characters remaining: " + MaxMsg.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Characters remaining: " + (MaxMsg - textBox1.Text.Length).ToString();
+        }
+
+        private void buttonObfuscate_Click(object sender, EventArgs e)
+        {
+            if (textBoxSender.Text.Length < 1)
+            { MessageBox.Show("Please use an encryption key of at least one character."); goto end; }
+            
+            if (textBox1.Text.Length < 1 || textBox1.Text.Length > MaxMsg)
+            { MessageBox.Show("Message length must be between 1 and " + MaxMsg.ToString() + " characters."); goto end; }
+            // Ready to encrypt
+            textBox4.Text = encrypt(textBox1.Text, textBoxSender.Text +"obfuscator");
+            label6.Text = "Characters: " + textBox4.Text.Length.ToString();
+            
+        end: ;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            if (textBoxSender.Text.Length < 1)
+            { MessageBox.Show("Please use an encryption key of at least one character."); goto end; }
+
+            if (textBox1.Text.Length < 1 || textBox1.Text.Length > MaxMsg)
+            { MessageBox.Show("Message length must be between 1 and " + MaxMsg.ToString() + " characters."); goto end; }
+            // Ready to decrypt
+            textBox4.Text = decrypt(textBox1.Text, textBoxSender.Text + "obfuscator");
+            label6.Text = "Characters: " + textBox4.Text.Length.ToString();
+
+        end: ;
+        }
+
+  
+
+        private string encrypt(string input, string hash)
+        {
+            hash = GetMD5Hash(hash);
+            string output = "";
+            int index = 0;
+            foreach (char c in input)
+            {   if (index >= hash.Length) { index = 0; }
+                byte b = Convert.ToByte(Convert.ToChar(hash.Substring(index, 1)));
+                index++;
+                if (b < 58) { b -= 47; } // convert ascii numbers into numbers 1-10
+                if (b > 64) { b -= 86; } // converv ascii hex A-F into numbers 11-16
+                string upperCase = "";
+                byte tempc = Convert.ToByte(c);
+                if (tempc > 47 && tempc < 58) { upperCase = "N"; } // number
+                if (tempc > 64 && tempc < 91) { upperCase = "U"; } // upper
+                if (tempc > 96 && tempc < 123) { upperCase = "L"; } // lower
+                if (upperCase.Length > 0 ) { tempc += b; } 
+                if (upperCase == "N" && tempc > 57) { tempc -= 10; }
+                if (upperCase == "N" && tempc > 57) { tempc -= 10; } // repeat once
+                if (upperCase == "U" && tempc > 90) { tempc -= 26; }
+                if (upperCase == "L" && tempc > 122) { tempc -= 26; }
+                output += Convert.ToChar(tempc);
+           }
+            return output;
+
+        }
+        private string decrypt(string input, string hash)
+        {
+            hash = GetMD5Hash(hash);
+            string output = "";
+            int index = 0;
+            foreach (char c in input)
+            {   if (index >= hash.Length) { index = 0; }
+                byte b = Convert.ToByte(Convert.ToChar(hash.Substring(index, 1)));
+                index++;
+                if (b < 58) { b -= 47; } // convert ascii numbers into numbers 1-10
+                if (b > 64) { b -= 86; } // converv ascii hex A-F into numbers 11-16
+                
+                string upperCase = "";
+                byte tempc = Convert.ToByte(c);
+                if (tempc > 47 && tempc < 58) { upperCase = "N"; }
+                if (tempc > 64 && tempc < 91) { upperCase = "U"; }
+                if (tempc > 96 && tempc < 123) { upperCase = "L"; }
+                if (upperCase.Length > 0 ) { tempc -= b; }
+                if (upperCase == "N" && tempc < 48) { tempc += 10; }
+                if (upperCase == "N" && tempc < 48) { tempc += 10; } // repeat once
+                if (upperCase == "U" && tempc < 65) { tempc += 26; }
+                if (upperCase == "L" && tempc < 97) { tempc += 26; }
+                output += Convert.ToChar(tempc);
+            }
+
+            return output;
+        }
+
+
+
+        public string GetMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bs = System.Text.Encoding.UTF8.GetBytes(input);
+            bs = x.ComputeHash(bs);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+     
+    }
+}
